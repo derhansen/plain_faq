72ccf4114dc695939feaee3191098250c84aef00
Torben Hansen <derhansen@gmail.com>
2021-10-01 16:16:07 +0200

    [TASK] Add softref config for field "answer"
    Refs #54

--- Configuration/TCA/tx_plainfaq_domain_model_faq.php
+++ Configuration/TCA/tx_plainfaq_domain_model_faq.php
@@ -201,8 +201,8 @@ return [
                 'cols' => 40,
                 'rows' => 15,
                 'eval' => 'trim',
+                'softref' => 'rtehtmlarea_images,typolink_tag,images,email[subst],url',
             ],
-
         ],
         'keywords' => [
             'exclude' => true,
