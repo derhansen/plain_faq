8aa5274a931545efa1b7d697a650630e6376436f
Torben Hansen <derhansen@gmail.com>
2021-10-18 18:51:24 +0200

    [TASK] Remove management DB fields from ext_tables.sql
    Refs #48

--- ext_tables.sql
+++ ext_tables.sql
@@ -2,10 +2,6 @@
 # Table structure for table 'tx_plainfaq_domain_model_faq'
 #
 CREATE TABLE tx_plainfaq_domain_model_faq (
-
-	uid int(11) NOT NULL auto_increment,
-	pid int(11) DEFAULT '0' NOT NULL,
-
 	question varchar(255) DEFAULT '' NOT NULL,
 	answer text,
 	keywords text,
@@ -16,50 +12,7 @@ CREATE TABLE tx_plainfaq_domain_model_faq (
 	slug varchar(2048),
 	faq_import_id varchar(100) DEFAULT '' NOT NULL,
 
-	tstamp int(11) unsigned DEFAULT '0' NOT NULL,
-	crdate int(11) unsigned DEFAULT '0' NOT NULL,
-	cruser_id int(11) unsigned DEFAULT '0' NOT NULL,
-	deleted smallint(5) unsigned DEFAULT '0' NOT NULL,
-	hidden smallint(5) unsigned DEFAULT '0' NOT NULL,
-	starttime int(11) unsigned DEFAULT '0' NOT NULL,
-	endtime int(11) unsigned DEFAULT '0' NOT NULL,
-	fe_group varchar(100) DEFAULT '0' NOT NULL,
-
-	t3ver_oid int(11) DEFAULT '0' NOT NULL,
-	t3ver_id int(11) DEFAULT '0' NOT NULL,
-	t3ver_wsid int(11) DEFAULT '0' NOT NULL,
-	t3ver_label varchar(255) DEFAULT '' NOT NULL,
-	t3ver_state smallint(6) DEFAULT '0' NOT NULL,
-	t3ver_stage int(11) DEFAULT '0' NOT NULL,
-	t3ver_count int(11) DEFAULT '0' NOT NULL,
-	t3ver_tstamp int(11) DEFAULT '0' NOT NULL,
-	t3ver_move_id int(11) DEFAULT '0' NOT NULL,
-	t3_origuid int(11) DEFAULT '0' NOT NULL,
-	sorting int(11) DEFAULT '0' NOT NULL,
-
-	sys_language_uid int(11) DEFAULT '0' NOT NULL,
-	l10n_parent int(11) DEFAULT '0' NOT NULL,
-	l10n_diffsource mediumblob,
-	l10n_state text,
-
-	PRIMARY KEY (uid),
-	KEY parent (pid),
-	KEY t3ver_oid (t3ver_oid,t3ver_wsid),
-	KEY language (l10n_parent,sys_language_uid),
 	KEY faqimport (faq_import_id)
-
-);
-
-#
-# Table structure for table 'tx_plainfaq_domain_model_faq_related_mm'
-#
-CREATE TABLE tx_plainfaq_domain_model_faq_related_mm (
-	uid_local int(11) DEFAULT '0' NOT NULL,
-	uid_foreign int(11) DEFAULT '0' NOT NULL,
-	sorting int(11) DEFAULT '0' NOT NULL,
-	sorting_foreign int(11) DEFAULT '0' NOT NULL,
-	KEY uid_local (uid_local),
-	KEY uid_foreign (uid_foreign)
 );
 
 #
