5941f62cbd3febeee1fe213e8f307bf5a2201073
Torben Hansen <derhansen@gmail.com>
2024-11-03 14:00:33 +0100

    [!!!][TASK] Remove migrate Commands for ext:irfaq migration
    Closes #89

--- ext_tables.sql
+++ ext_tables.sql
@@ -10,16 +10,4 @@ CREATE TABLE tx_plainfaq_domain_model_faq (
 	categories int(11) unsigned DEFAULT '0' NOT NULL,
 	related int(11) DEFAULT '0' NOT NULL,
 	slug varchar(2048),
-	faq_import_id varchar(100) DEFAULT '' NOT NULL,
-
-	KEY faqimport (faq_import_id)
-);
-
-#
-# Extend table structure of table 'sys_category'
-#
-CREATE TABLE sys_category (
-	faq_import_id varchar(100) DEFAULT '' NOT NULL,
-
-	KEY faqimport (faq_import_id)
 );
